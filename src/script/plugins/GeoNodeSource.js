f2e65eeb5291271f943db234f31605e6d5e457ff
Matt Bertrand <mbertrand@cga.harvard.edu>
2011-04-29 16:48:58 -0400

    Set tiled=true

--- src/script/plugins/GeoNodeSource.js
+++ src/script/plugins/GeoNodeSource.js
@@ -66,6 +66,8 @@ gxp.plugins.GeoNodeSource = Ext.extend(gxp.plugins.LayerSource, {
                 maxExtent = undefined;
             }
 
+        
+
             var params = {
                 STYLES: config.styles,
                 FORMAT: config.format,
@@ -94,6 +96,20 @@ gxp.plugins.GeoNodeSource = Ext.extend(gxp.plugins.LayerSource, {
                 }
             );
 
+            if ("tiled" in config && config.tiled == true) {
+
+                var tileWidth = config['tileWidth'] || 256;
+                var tileHeight = config['tileHeight'] || 256;
+                var tileResolutions = config['tileResolutions']  || [156543.03390625,78271.516953125,39135.7584765625,19567.87923828125,9783.939619140625,4891.9698095703125,2445.9849047851562,1222.9924523925781,611.4962261962891,305.74811309814453,152.87405654907226,76.43702827453613,38.218514137268066,19.109257068634033,9.554628534317017,4.777314267158508,2.388657133579254,1.194328566789627,0.5971642833948135,0.29858214169740677,0.14929107084870338,0.07464553542435169,0.037322767712175846,0.018661383856087923,0.009330691928043961,0.004665345964021981];
+                var originLon = config['tileOriginLon'] || -20037508.34;
+                var originLat = config['tileOriginLat'] || -20037508.34;
+
+                  layer.addOptions({resolutions: tileResolutions,
+                  tileSize: new OpenLayers.Size(tileWidth, tileHeight),
+                  tileOrigin: new OpenLayers.LonLat(originLat, originLon)});
+                  console.log('TileResolutions:' + tileResolutions);
+                  layer.params.TILED = true; // set to true when http://projects.opengeo.org/suite/ticket/1286 is closed
+            }
             // data for the new record
             var data = {
                 title: config.title,
@@ -128,8 +144,6 @@ gxp.plugins.GeoNodeSource = Ext.extend(gxp.plugins.LayerSource, {
             var Record = GeoExt.data.LayerRecord.create(fields);
             record = new Record(data, layer.id);
 
-
-
             return record;
     },
 
@@ -147,9 +161,8 @@ gxp.plugins.GeoNodeSource = Ext.extend(gxp.plugins.LayerSource, {
         var layer = record.getLayer();
         var params = layer.params;
         return Ext.apply(config, {
-            format: params.FORMAT,
             styles: params.STYLES,
-            transparent: params.TRANSPARENT
+            tiled: record.getLayer().params.TILED
         });
     }    
 
