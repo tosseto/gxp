165ac5686a745004782f6c88159448a8f664a18b
Matt Bertrand <mbertrand@cga.harvard.edu>
2012-02-14 11:04:18 -0600

    Fix typo

--- src/script/plugins/WMSSource.js
+++ src/script/plugins/WMSSource.js
@@ -268,7 +268,7 @@ gxp.plugins.WMSSource = Ext.extend(gxp.plugins.LayerSource, {
             SERVICE: "WMS",
             REQUEST: "GetCapabilities",
             VERSION: "1.1.1",
-            RANDOMIZE:  + "rnd" + Math.floor(Math.random()*10001) //Prevent IE from caching results
+            RANDOMIZE: "rnd" + Math.floor(Math.random()*10001) //Prevent IE from caching results
         };
         if (this.version) {
             baseParams.VERSION = this.version;
